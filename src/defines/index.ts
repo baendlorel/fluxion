d8497e6ee9061d2f36a7248a0fd6271dbf887263
aldia_mechdream <futami16237@gmail.com>
2026-06-23 16:50:57 +0800

    feat: 添加定义Fluxion模块和中间件的函数，增强类型安全性

---
+++ src/defines/index.ts
@@ -0,0 +1,64 @@
+import { FluxionModuleType } from '@/common/consts';
+import { FluxionLoggerFn } from '@/common/logger';
+import { FluxionHandler, FluxionDispose, FluxionModuleWithType, FluxionModule, FluxionMiddleware } from '@/types';
+
+/**
+ * Use handler function and optional disposer function to define a Fluxion module.
+ * @param handler Main function that handles request and response instances
+ * @param disposer Deal with resource cleanup when the server is about to close
+ */
+export function defineFluxionModule(handler: FluxionHandler, disposer?: FluxionDispose): FluxionModuleWithType;
+/**
+ * Provides type safety for defining Fluxion modules.
+ */
+export function defineFluxionModule(fluxionModule: FluxionModule): FluxionModuleWithType;
+export function defineFluxionModule(
+  a: FluxionModule | FluxionHandler,
+  disposer?: FluxionDispose,
+): FluxionModuleWithType {
+  if (typeof a === 'function') {
+    if (typeof disposer !== 'function') {
+      $throw(`Invalid disposer, expected a function but got ${typeof disposer}`);
+    }
+    return { handler: a, disposer, type: FluxionModuleType.Api };
+  }
+
+  if (typeof a !== 'object' || a === null) {
+    $throw(`Invalid argument, expected a FluxionModule object or a handler function, but got ${typeof a}`);
+  }
+
+  if (typeof a.handler !== 'function') {
+    $throw(`Invalid FluxionModule, "handler" must be a function`);
+  }
+
+  if (a.disposer !== undefined && typeof a.disposer !== 'function') {
+    $throw(`Invalid FluxionModule, "disposer" must be a function if provided`);
+  }
+
+  if (a.methods !== undefined && (!Array.isArray(a.methods) || a.methods.some((v) => typeof v !== 'string'))) {
+    $throw(`Invalid FluxionModule, "methods" must be an array of strings if provided`);
+  }
+
+  if (
+    a.middlewares !== undefined &&
+    (!Array.isArray(a.middlewares) || a.middlewares.some((v) => typeof v !== 'function'))
+  ) {
+    $throw(`Invalid FluxionModule, "middlewares" must be an array of functions if provided`);
+  }
+
+  return { ...a, type: FluxionModuleType.Api };
+}
+
+export function defineFluxionMiddleware(middleware: FluxionMiddleware): FluxionMiddleware {
+  if (typeof middleware !== 'function') {
+    $throw(`Invalid FluxionMiddleware, expected a function but got ${typeof middleware}`);
+  }
+  return middleware;
+}
+
+export function defineFluxionLogger(loggerFn: FluxionLoggerFn) {
+  if (typeof loggerFn !== 'function') {
+    $throw(`Invalid FluxionLoggerFn, expected a function but got ${typeof loggerFn}`);
+  }
+  return loggerFn;
+}
--- src/index.ts
+++ src/index.ts
@@ -33,61 +33,9 @@ export {
   GatewayTimeoutException,
 } from './http/exceptions.js';
 
+export * from './defines/index.js';
+
 export type { FluxionDispose, FluxionHandler, FluxionModule as FluxionHandlerModule, FluxionOptions } from './types.js';
-
-/**
- * Use handler function and optional disposer function to define a Fluxion module.
- * @param handler Main function that handles request and response instances
- * @param disposer Deal with resource cleanup when the server is about to close
- */
-export function defineFluxionModule(handler: FluxionHandler, disposer?: FluxionDispose): FluxionModuleWithType;
-/**
- * Provides type safety for defining Fluxion modules.
- */
-export function defineFluxionModule(fluxionModule: FluxionModule): FluxionModuleWithType;
-export function defineFluxionModule(
-  a: FluxionModule | FluxionHandler,
-  disposer: FluxionDispose = noop,
-): FluxionModuleWithType {
-  if (typeof a === 'function') {
-    if (typeof disposer !== 'function') {
-      $throw(`Invalid disposer, expected a function but got ${typeof disposer}`);
-    }
-    return { handler: a, disposer, type: FluxionModuleType.Api };
-  }
-
-  if (typeof a !== 'object' || a === null) {
-    $throw(`Invalid argument, expected a FluxionModule object or a handler function, but got ${typeof a}`);
-  }
-
-  if (typeof a.handler !== 'function') {
-    $throw(`Invalid FluxionModule, "handler" must be a function`);
-  }
-
-  if (a.disposer !== undefined && typeof a.disposer !== 'function') {
-    $throw(`Invalid FluxionModule, "disposer" must be a function if provided`);
-  }
-
-  if (a.methods !== undefined && (!Array.isArray(a.methods) || a.methods.some((v) => typeof v !== 'string'))) {
-    $throw(`Invalid FluxionModule, "methods" must be an array of strings if provided`);
-  }
-
-  if (
-    a.middlewares !== undefined &&
-    (!Array.isArray(a.middlewares) || a.middlewares.some((v) => typeof v !== 'function'))
-  ) {
-    $throw(`Invalid FluxionModule, "middlewares" must be an array of functions if provided`);
-  }
-
-  return { ...a, type: FluxionModuleType.Api };
-}
-
-export function defineFluxionMiddleware(middleware: FluxionMiddleware): FluxionMiddleware {
-  if (typeof middleware !== 'function') {
-    $throw(`Invalid FluxionMiddleware, expected a function but got ${typeof middleware}`);
-  }
-  return middleware;
-}
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.$throw = (message: string) => {
