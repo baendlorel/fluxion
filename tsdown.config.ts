c6c5a3efe0eff529b12462afb3d03f1955df82dc
aldia_mechdream <futami16237@gmail.com>
2026-06-25 17:06:12 +0800

    feat: 更新 package.json 和 tsdown.config.ts，调整入口文件和类型定义路径

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,34 +2,51 @@ import { defineConfig } from 'tsdown';
 import replace from '@rollup/plugin-replace';
 
 const isDev = process.env.NODE_ENV === 'development';
+const plugins = () => [
+  replace({
+    preventAssignment: true,
+    delimiters: ['', ''],
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 
-export default defineConfig({
-  entry: ['src/index.ts', 'src/cli/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
-  clean: true,
-  sourcemap: true,
-  minify: !isDev,
-  target: 'node24',
-  exports: {
-    bin: {
-      fluxion: './src/cli.ts',
+    // global $throw
+    "$throw('": `throw new Error('[fluxion error] `,
+    '$throw(`': 'throw new Error(`[fluxion error] ',
+    '$throw("': `throw new Error("[fluxion error] `,
+  }),
+];
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    minify: !isDev,
+    target: 'node24',
+    treeshake: !isDev,
+    plugins: plugins(),
+    deps: {
+      onlyBundle: ['type-narrow', 'fast-json-stable-stringify'],
     },
   },
-  treeshake: !isDev,
-  plugins: [
-    replace({
-      preventAssignment: true,
-      delimiters: ['', ''],
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-
-      // global $throw
-      "$throw('": `throw new Error('[fluxion error] `,
-      '$throw(`': 'throw new Error(`[fluxion error] ',
-      '$throw("': `throw new Error("[fluxion error] `,
-    }),
-  ],
-  deps: {
-    onlyBundle: ['type-narrow', 'fast-json-stable-stringify'],
+  {
+    entry: { cli: 'src/cli/index.ts' },
+    format: ['esm', 'cjs'],
+    dts: false,
+    clean: true,
+    sourcemap: false,
+    minify: !isDev,
+    target: 'node24',
+    exports: {
+      bin: {
+        fluxion: 'src/cli/index.ts',
+      },
+    },
+    treeshake: !isDev,
+    plugins: plugins(),
+    deps: {
+      onlyBundle: ['type-narrow', 'fast-json-stable-stringify'],
+    },
   },
-});
+]);
