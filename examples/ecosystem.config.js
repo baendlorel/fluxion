cc1a35ebd218ef4490d10f4fc9f723c36ad9adb2
louie <futami16237@gmail.com>
2026-08-06 22:50:56 +0800

    chore 未能复现pm2 restart异常

--- examples/ecosystem.config.js
+++ examples/ecosystem.config.js
@@ -1,3 +1,8 @@
+const { homedir } = require('os');
+const { execSync } = require('child_process');
+
+const tsxPath = execSync('which tsx').toString().trim(); // got '<paths>/bin/tsx\n' so we need to trim it.
+
 module.exports = {
-  apps: [{ name: 'app', script: './examples/main.ts', interpreter: '/home/aldia/.nvm/versions/node/v24.5.0/bin/tsx' }],
+  apps: [{ name: 'app', script: './main.ts', interpreter: tsxPath }],
 };
